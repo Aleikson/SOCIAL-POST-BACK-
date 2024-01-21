8b78076e93e4e6e94c69716ea45de29d40468bd2
Aleikson <aleiksonsilva@hotmail.com>
2024-01-20 21:09:10 -0300

    Added place specific route

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,12 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (request, response, next) => {
+const TEST = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u1'
+    }
+];
 
-    console.log('Test request');
-
-    response.json({ message: 'Working' });
-
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid;
+    const place = TEST.find(p => {
+        return p.id === placeId;
+    });
+    res.json({ place });
 });
 
 module.exports = router;
